9c5f9f7537edf157dfb437b94c35930565e250ff
seherkose <seherkose1@outlook.com>
2023-08-09 00:08:05 +0300

    Initial Commit

---
+++ LetterboxdUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetterboxdUI
+//
+//  Created by Seher Köse on 9.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
